84f845a390cf805214e9575b7206f4078796b865
yannikkellerde <yannik@kelnet.de>
2020-10-28 14:36:31 +0100

    sync

--- game_creator/client/js/slider.js
+++ game_creator/client/js/slider.js
@@ -1,14 +1,15 @@
 const inputs = document.querySelectorAll(".slider_input");
 const labels = document.querySelectorAll(".slider_label");
 
-for (var i=0;i<=inputs.length;i++){
+console.log(inputs)
+for (var i=0;i<inputs.length;i++){
     const input = inputs[i];
     const label = labels[i];
     const maxval = Number(input.getAttribute("data-max"));
     const eventfunc = event => {
         const value = Number(input.value) / 100;
         input.style.setProperty("--thumb-rotate", `${value * 720}deg`);
-        const real_val = Math.round(value * maxval)
+        const real_val = Math.round(value * maxval+1)
         label.innerHTML = real_val;
         input.setAttribute("data-value",real_val)
     }
