85e70d3e548f704c67e748f59727114acab49ab3
yannikkellerde <yannik@kelnet.de>
2020-11-03 00:54:21 +0100

    not alot of progress

--- python_server/server_graph.py
+++ python_server/server_graph.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("..")
 sys.path.append(".")
-from graph_tools_games import Tic_tac_toe, Qango6x6, Qango7x7, Qango7x7_plus
+from graph_tools_games import Tic_tac_toe, Qango6x6, Qango7x7, Qango7x7_plus, Json_game
 import math
 import pickle
 import json
@@ -12,6 +12,8 @@ import webbrowser
 from http.server import HTTPServer,SimpleHTTPRequestHandler
 from functools import reduce
 import os
+
+base_path = os.path.abspath(os.path.dirname(__file__))
 
 FILE = "python_server/explore_wins.html"
 PORT = 8081
@@ -57,18 +59,23 @@ class Post_handler(SimpleHTTPRequestHandler):
         length = int(self.headers['Content-Length'])
         data_str = self.rfile.read(length)
         data = json.loads(data_str)
-        real_pos = [("f" if x==0 else ("b" if x==2 else "w")) for x in data["position"]]
-        game.board.set_position(real_pos,"b" if data["onturn"]==1 else "w")
-        game.board.draw_me()
-        depth = len(list(filter(lambda x:x!="f",real_pos)))
-        moves = game.get_actions(filter_superseeded=False,none_for_win=False)
-        board_moves = [game.board.node_map[x] for x in moves]
-        game.draw_me()
-        evals = game.board.check_move_val(moves)
-        moves_with_eval = list(zip(board_moves, evals))
-        # send the message back
-        self._set_headers()
-        self.wfile.write(json.dumps({"moves":moves_with_eval}).encode())
+        if "request" in data:
+            self._set_headers()
+            self.wfile.write(json.dumps(game.config).encode())
+        else:
+            real_pos = [("f" if x==0 else ("b" if x==2 else "w")) for x in data["position"]]
+            print(real_pos,game.board.position)
+            game.board.set_position(real_pos,"b" if data["onturn"]==1 else "w")
+            game.board.draw_me()
+            depth = len(list(filter(lambda x:x!="f",real_pos)))
+            moves = game.get_actions(filter_superseeded=False,none_for_win=False)
+            board_moves = [game.board.node_map[x] for x in moves]
+            game.draw_me()
+            evals = game.board.check_move_val(moves)
+            moves_with_eval = list(zip(board_moves, evals))
+            # send the message back
+            self._set_headers()
+            self.wfile.write(json.dumps({"moves":moves_with_eval}).encode())
 
 def open_browser():
     """Start a browser after waiting for half a second."""
@@ -91,6 +98,8 @@ def start_server():
 
 if __name__ == "__main__":
     my_folder = sys.argv[1]
+    game_name = my_folder
+    start_arg = 2
     if "qango6x6"==my_folder:
         game = Qango6x6()
     elif "qango7x7"==my_folder:
@@ -99,24 +108,28 @@ if __name__ == "__main__":
         game = Tic_tac_toe()
     elif my_folder == "qango7x7_plus":
         game = Qango7x7_plus()
+    elif my_folder == "json":
+        game = Json_game(os.path.join(base_path,"../json_games",sys.argv[2]+".json"))
+        game_name = sys.argv[2]
+        start_arg = 3
     else:
         raise ValueError(f"Game not found {my_folder}")
     try:
-        if len(sys.argv)>=3:
-            game.board.load_sets(provenfile_black=f"../proofsets/b_{my_folder}_{sys.argv[2]}p.pkl",
-                                 disprovenfile_black=f"../proofsets/b_{my_folder}_{sys.argv[2]}d.pkl")
+        if len(sys.argv)>start_arg:
+            game.board.load_sets(provenfile_black=f"../proofsets/b_{game_name}_{sys.argv[start_arg]}p.pkl",
+                                 disprovenfile_black=f"../proofsets/b_{game_name}_{sys.argv[start_arg]}d.pkl")
         else:
-            game.board.load_sets(provenfile_black=f"../proofsets/full_game/b_{my_folder}_p.pkl",
-                                 disprovenfile_black=f"../proofsets/full_game/b_{my_folder}_d.pkl")
+            game.board.load_sets(provenfile_black=f"../proofsets/full_game/b_{game_name}_p.pkl",
+                                 disprovenfile_black=f"../proofsets/full_game/b_{game_name}_d.pkl")
     except FileNotFoundError as e:
         print(e)
     try:
-        if len(sys.argv)>=3:
-            game.board.load_sets(provenfile_white=f"../proofsets/w_{my_folder}_{sys.argv[2]}p.pkl",
-                                 disprovenfile_white=f"../proofsets/w_{my_folder}_{sys.argv[2]}d.pkl")
+        if len(sys.argv)>start_arg:
+            game.board.load_sets(provenfile_white=f"../proofsets/w_{game_name}_{sys.argv[start_arg]}p.pkl",
+                                 disprovenfile_white=f"../proofsets/w_{game_name}_{sys.argv[start_arg]}d.pkl")
         else:
-            game.board.load_sets(provenfile_white=f"../proofsets/full_game/w_{my_folder}_p.pkl",
-                                 disprovenfile_white=f"../proofsets/full_game/w_{my_folder}_d.pkl")
+            game.board.load_sets(provenfile_white=f"../proofsets/full_game/w_{game_name}_p.pkl",
+                                 disprovenfile_white=f"../proofsets/full_game/w_{game_name}_d.pkl")
     except FileNotFoundError as e:
         print(e)
     endgame_depth = 0
